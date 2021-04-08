23192e87edf668cd454664549066cdcf77cf6b33
M.dzhamalov <m_djamalov@mail.ru>
2021-04-08 17:34:07 +0300

    work on DataHelper.java

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -7,7 +7,19 @@ public class DataHelper {
     }
 
     @Value
-    public static class VerificationCode {
+    public static class Cards {
         private String card;
     }
+    public static Cards getVerificationCard () {
+        return new Cards("4444 4444 4444 4441");
+    }
+    public static Cards getInvalidCard () {
+        return new Cards("4444 4444 4444 4442");
+    }
+
+    @Value
+    public static class Name {
+
+    }
+
 }
