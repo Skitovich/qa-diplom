f2ebba1eafd39a28d1f69496242f847911e1e8fc
M.dzhamalov <m_djamalov@mail.ru>
2021-04-15 16:40:35 +0300

    work on DataHelper.java 15.04.

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,14 +1,19 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
 import lombok.Value;
+
+import java.util.Locale;
 
 public class DataHelper {
     private DataHelper() {
     }
 
+    Faker faker = new Faker();
+
     @Value
     public static class Cards {
-        private String card;
+        String card;
     }
     public static Cards getVerificationCard () {
         return new Cards("4444 4444 4444 4441");
@@ -18,8 +23,54 @@ public class DataHelper {
     }
 
     @Value
-    public static class Name {
-
+    public static class OwnerName {
+    String ownerCardName;
     }
+
+    public static OwnerName getValidOwnerCardName () {
+        return new OwnerName("Ruslan");
+    }
+    public static OwnerName getInvalidOwnerCardName () {
+        return new OwnerName("%?*(12123");
+    }
+
+    @Value
+    public static class MonthCardExpiration {
+        String month;
+    }
+
+    public static MonthCardExpiration getValidMonth () {
+        return new MonthCardExpiration("08");
+    }
+    public static MonthCardExpiration getInvalidMonth () {
+        return new MonthCardExpiration("RA");
+    }
+
+
+    @Value
+    public static class YearCardExpiration {
+        String year;
+    }
+
+    public static YearCardExpiration getValidYearCardExpiration () {
+        return new YearCardExpiration("22");
+    }
+    public static YearCardExpiration getInvalidYearCardExpiration () {
+        return new YearCardExpiration("RA");
+    }
+
+    @Value
+    public static class CvcCvvCard {
+        String cvc;
+    }
+
+    public static YearCardExpiration getValidYearCvcCvvCard () {
+        return new YearCardExpiration("999");
+    }
+    public static YearCardExpiration getInvalidYearCvcCvvCard () {
+        return new YearCardExpiration("RRA");
+    }
+
+
 
 }
